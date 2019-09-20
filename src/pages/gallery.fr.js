61da4749595755040fa8b29cfdbce1777ab7a963
zifan <zuoduzuodu24@gmail.com>
2019-09-20 02:31:34 +0200

    Revert "use single component for pages"
    This reverts commit 3af666f6

--- src/pages/gallery.fr.js
+++ src/pages/gallery.fr.js
@@ -1,12 +1,68 @@
 import React from 'react';
-import { SEO } from 'Common';
+import { SEO, GalleryDisplay } from 'Common';
 import { LayoutFr } from 'Layout';
-import { MainGalleryFr } from "Components/gallery/MainGalleryFr";
+import { Heading, Txt, Box } from 'rendition';
+import {
+  GalleryWrapper,
+  ContentWrapper,
+  PaddedWrapper,
+  CenteredWrapper,
+} from '../themes/pageStyles';
 
 // Images from svgbackgrounds.com and heropatterns.com
 export default props => (
-	<LayoutFr location={props.location}>
-		<SEO title="Galerie photos" location="/fr/gallery" />
-		<MainGalleryFr />
-	</LayoutFr>
+  <LayoutFr location={props.location}>
+    <SEO title="Galerie photos" location="/fr/gallery" />
+    <PaddedWrapper>
+      <CenteredWrapper>
+        <Heading.h1 pt={5} pb={3}>
+          Galerie photo&nbsp;
+          <span role="img" aria-label="camera">
+            📷
+          </span>
+        </Heading.h1>
+        <Box style={{ textAlign: 'justify' }} pb={5}>
+          <Txt>
+            À mes heures perdues, je me consacre à la photographie. J'ai comme
+            appareil un SONY Alpha III avec plusieurs optiques. J'adore la
+            macrophotographie et j'apprends petit à petit comment réaliser de
+            belles prises. Je me balade aussi avec mon appareil lors des voyages
+            et j'en profite pour prendre des prises sympas.
+          </Txt>
+          <Txt pt={2}>
+            Si les photos de cette galerie vous intéressent, contactez moi via
+            la page contact !
+          </Txt>
+        </Box>
+      </CenteredWrapper>
+    </PaddedWrapper>
+    <GalleryWrapper
+      bgImage="/illustrations/endless-clouds.svg"
+      bgColor="#003171"
+      zIndex={4}
+    >
+      <ContentWrapper>
+        <GalleryDisplay cat="japan" lang="fr" />
+      </ContentWrapper>
+    </GalleryWrapper>
+    <GalleryWrapper
+      bgImage="/illustrations/plus.svg"
+      bgColor="#400000"
+      zIndex={3}
+    >
+      <ContentWrapper>
+        <GalleryDisplay cat="macro" lang="fr" />
+      </ContentWrapper>
+    </GalleryWrapper>
+    <GalleryWrapper
+      bgImage="/illustrations/squares.svg"
+      bgColor="#787878"
+      zIndex={2}
+    >
+      <ContentWrapper>
+        <GalleryDisplay cat="everythingelse" lang="fr" />
+      </ContentWrapper>
+    </GalleryWrapper>
+    <ContentWrapper />
+  </LayoutFr>
 );
--- src/pages/index.en.js
+++ src/pages/index.en.js
@@ -4,11 +4,11 @@ import { LayoutEn } from 'Layout';
 import { Intro, About, ContactEn, Projects } from 'Components/landing';
 
 export default props => (
-	<LayoutEn location={props.location}>
-		<SEO title="Zifan's blog" location="/en" />
-		<Intro />
-		<About lang="en" />
-		<Projects lang="en" />
-		<ContactEn />
-	</LayoutEn>
+  <LayoutEn location={props.location}>
+    <SEO title="Zifan's blog" location="/en" />
+    <Intro />
+    <About lang="en" />
+    <Projects lang="en" />
+    <ContactEn />
+  </LayoutEn>
 );
